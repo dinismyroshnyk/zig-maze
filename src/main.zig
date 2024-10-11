597b646ffa699cd2019516032d44f34827bbe232
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-10-11 23:25:40 +0100

    Moovable "player"

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,40 @@ const std = @import("std");
 const raylib = @import("raylib.zig");
 
 pub fn main() !void {
-    const screenWidth = 1500;
-    const screenHeight = 720;
+    const screen_width = 1500;
+    const screen_height = 720;
+    const cell_size = 50;
 
-    raylib.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    const Player = struct {
+        x: c_int,
+        y: c_int,
+    };
+
+    var player = Player {
+        .x = screen_width / 2,
+        .y = screen_height / 2,
+    };
+
+    raylib.InitWindow(screen_width, screen_height, "zig-maze");
     //raylib.SetTargetFPS(60);
 
     while (!raylib.WindowShouldClose()) {
+        if (raylib.IsKeyPressed(raylib.KEY_RIGHT) or raylib.IsKeyPressed(raylib.KEY_D)) {
+            player.x += cell_size;
+        } else if (raylib.IsKeyPressed(raylib.KEY_LEFT) or raylib.IsKeyPressed(raylib.KEY_A)) {
+            player.x -= cell_size;
+        } else if (raylib.IsKeyPressed(raylib.KEY_UP) or raylib.IsKeyPressed(raylib.KEY_W)) {
+            player.y -= cell_size;
+        } else if (raylib.IsKeyPressed(raylib.KEY_DOWN) or raylib.IsKeyPressed(raylib.KEY_S)) {
+            player.y += cell_size;
+        }
+
         raylib.BeginDrawing();
         raylib.ClearBackground(raylib.RAYWHITE);
-        raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, raylib.LIGHTGRAY);
-        raylib.DrawFPS(screenWidth - 100, 10);
+
+        raylib.DrawRectangle(player.x, player.y, cell_size, cell_size, raylib.RED);
+
+        raylib.DrawFPS(screen_width - 100, 10);
         raylib.EndDrawing();
     }
 
